3761181fe4c7d35bf12b4d87f56c9513094867a7
Jula <juliamos45@gmail.com>
2019-11-23 12:52:26 +0100

    Jula mapa

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 
-class About extends Component {
+const Map = ReactMapboxGl({
+  accessToken:
+    'pk.eyJ1Ijoiam1vczEyMyIsImEiOiJjazNiaTRjcnEwbTJ5M2RrM2ZkZWxwazN0In0.IuK6kWPBjc8lq2nHbkw0QA'
+});
+
+class About extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      lng: 52.247012,
+      lat: 21.062927,
+      zoom: 13
+    }
+  }
+
   render() {
+    const position = [this.state.lat, this.state.lng];
     return (
-      <div> ABOUT </div>
+      <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        center = {position}
+        containerStyle={{
+          height: '100vh',
+          width: '100vw'
+        }}
+      >
+        <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+          <Feature coordinates={[52.247012,21.062927]} />
+        </Layer>
+      </Map>
     );
   }
 }
+
 
 export default About;
