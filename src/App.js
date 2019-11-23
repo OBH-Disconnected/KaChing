3cd145b234eb6f1d0bc87e3cce09aafb7831db34
Jula <juliamos45@gmail.com>
2019-11-23 10:27:37 +0100

    done something

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import Login from "./components/Login"
-import Main from "./components/Main"
+import Main from "./components/Main";
+import About from "./components/About"
+import Users from "./components/Users"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
@@ -15,7 +25,37 @@ class App extends Component {
   render() {
     return (
       this.state.logged ?
-      <Main />
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Main</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Main />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
       :
       <Login login={this.loginUser}/>
     );
