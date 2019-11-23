80030272dbffa4c0b776132bd7db02e363f7807b
Jula <juliamos45@gmail.com>
2019-11-23 10:48:04 +0100

    Jula logowanie

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,11 +6,23 @@ import { Form, Button } from 'react-bootstrap';
 class Login extends Component {
   constructor() {
     super();
+    this.state={
+      login:"",
+      password:""
+    }
+  }
+  changeLogin = (event) =>{
+    this.setState({login: event.target.value})
+  }
+  changePassword = (event) =>{
+    this.setState({password: event.target.value})
   }
   handleSubmit = (event) => {
     event.preventDefault()
-    console.log(event.target.value)
-    this.props.login();
+    this.state.login === "admin" && this.state.password==="admin"?
+    this.props.login()
+    :
+    console.log("nie")
   }
   render() {
     return (
@@ -19,12 +31,12 @@ class Login extends Component {
           <Form onSubmit={this.handleSubmit}>
             <Form.Group controlId="formLogin">
               <Form.Label>Login</Form.Label>
-              <Form.Control type="text" placeholder="Enter login" required />
+              <Form.Control type="text" placeholder="Enter login" value={this.state.login} onChange={this.changeLogin} required />
             </Form.Group>
 
             <Form.Group controlId="formPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password"  required />
+              <Form.Control type="password" placeholder="Password" value={this.state.password} onChange={this.changePassword} required />
             </Form.Group>
             <Button variant="primary" type="submit">
               Log In
